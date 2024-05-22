339ad652f5f0ef05f8afcc60a79277f1e5f54668
Gayatri Sharma <52161690+GayatriSharma23@users.noreply.github.com>
2024-05-22 13:01:09 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
-const Chat = ({ messages }) => {
+const Chat = ({ messages, loading }) => {
   return (
     <div className="chat">
       {messages.map((msg, index) => (
         <div key={index} className="message">
           <div className="user">{msg.user}</div>
-          <div className="bot">{msg.bot}</div>
+          <div className="bot">
+            {msg.bot}
+            <img src="laptop-icon.png" alt="Laptop" className="laptop-icon" />
+          </div>
         </div>
       ))}
+      {loading && (
+        <div className="loading-dots">
+          <span>.</span><span>.</span><span>.</span>
+        </div>
+      )}
     </div>
   );
 };
